1a8bd429ab2e172ea2cf1976edb70008eb090e8f
Sharryy <ibneadam388@gmail.com>
2021-09-14 13:36:22 +0500

    Anticipated Page Done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -199,14 +199,162 @@
                             </div>
                         </div>
                     </div>
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img src="ff7.jpg" alt="game cover"
+                                     class="hover:opacity-75 w-48 transition ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full"
+                                 style="right: -20px; bottom: -20px;">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                    80%
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-4">
+                                Final Fantasy 7 Remake
+                            </a>
+                            <div class="text-gray-400 mt-1">
+                                Playstation 1
+                            </div>
+                            <div class="mt-6 text-gray-400">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus, commodi
+                                cupiditate deserunt esse harum id ipsum natus nesciunt odit quasi qui recusandae
+                                voluptates. Aut cupiditate exercitationem in nemo. Amet blanditiis dolores laboriosam
+                                nemo neque possimus praesentium, quo quos repellat?
+                            </div>
+                        </div>
+                    </div>
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img src="ff7.jpg" alt="game cover"
+                                     class="hover:opacity-75 w-48 transition ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full"
+                                 style="right: -20px; bottom: -20px;">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                    80%
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-4">
+                                Final Fantasy 7 Remake
+                            </a>
+                            <div class="text-gray-400 mt-1">
+                                Playstation 1
+                            </div>
+                            <div class="mt-6 text-gray-400">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus, commodi
+                                cupiditate deserunt esse harum id ipsum natus nesciunt odit quasi qui recusandae
+                                voluptates. Aut cupiditate exercitationem in nemo. Amet blanditiis dolores laboriosam
+                                nemo neque possimus praesentium, quo quos repellat?
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="most-anticipated w-1/4">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam atque blanditiis commodi
-                consequuntur dolore dolorum ea hic incidunt iusto libero magni minima nulla officiis optio pariatur
-                perspiciatis placeat qui quod ratione suscipit, totam voluptatem. Aliquam assumenda at aut magnam, nulla
-                optio praesentium. Corporis cum deserunt doloremque ducimus ea, eius fugit incidunt inventore, molestias
-                omnis perferendis, placeat reiciendis temporibus vitae!
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Most Anticipated</h2>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="/cyberpunk.jpg" alt="game cover"
+                                 class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">CyberPunk 2077</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                        </div>
+                    </div>
+                </div><div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="/cyberpunk.jpg" alt="game cover"
+                                 class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">CyberPunk 2077</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                        </div>
+                    </div>
+                </div><div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="/cyberpunk.jpg" alt="game cover"
+                                 class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">CyberPunk 2077</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="/cyberpunk.jpg" alt="game cover"
+                                 class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">CyberPunk 2077</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                        </div>
+                    </div>
+                </div>
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold mt-8">Coming Soon</h2>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="/cyberpunk.jpg" alt="game cover"
+                                 class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">CyberPunk 2077</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="/cyberpunk.jpg" alt="game cover"
+                                 class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">CyberPunk 2077</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="/cyberpunk.jpg" alt="game cover"
+                                 class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">CyberPunk 2077</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="/cyberpunk.jpg" alt="game cover"
+                                 class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">CyberPunk 2077</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
