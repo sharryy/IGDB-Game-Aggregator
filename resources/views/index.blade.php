c3f5837193916db5bb7349cf7713f46fe7eafccc
Sharryy <ibneadam388@gmail.com>
2021-09-17 17:41:50 +0500

    Some UI Bugs DOne

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -109,10 +109,12 @@
                                      alt="game cover"
                                      class="w-16 hover:opacity-75 transition ease-in-out duration-150">
                             </a>
-                            <div class="ml-4">
-                                <a href="#" class="hover:text-gray-300">{{ $games['name'] }}</a>
-                                <div
-                                    class="text-gray-400 text-sm mt-1">{{ date('M j, Y.',$games['first_release_date']) }}</div>
+                            <div class="w-52">
+                                <div class="ml-4">
+                                    <a href="#" class="hover:text-gray-300">{{ $games['name'] }}</a>
+                                    <div
+                                        class="text-gray-400 text-sm mt-1">{{ date('M j, Y.',$games['first_release_date']) }}</div>
+                                </div>
                             </div>
                         </div>
                     </div>
