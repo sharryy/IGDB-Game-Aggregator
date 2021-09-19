72ee1bdc09a4b041d70c2c474e23fe121a6b0b3b
Sharryy <ibneadam388@gmail.com>
2021-09-20 02:53:50 +0500

    Skeleton Loader for Popular Games

--- resources/views/livewire/popular-games.blade.php
+++ resources/views/livewire/popular-games.blade.php
@@ -29,8 +29,18 @@
             </div>
         </div>
     @empty
-        <div class="spinner mt-8">
-            Loading..
-        </div>
+        @foreach(range(1, 12) as $game)
+            <div class="game mt-4">
+                <div class="relative inline-block">
+                    <div class="bg-gray-800 w-44 h-56"></div>
+                </div>
+                <div href="#" class="block text-transparent text-lg bg-gray-700 rounded leading-tight mt-4">
+                    Title Goes Here.
+                </div>
+                <div class="text-transparent bg-gray-700 inline-block rounded mt-3">
+                    PS4, PC, Switch
+                </div>
+            </div>
+        @endforeach
     @endforelse
 </div>
