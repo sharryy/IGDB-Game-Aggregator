0af47ea969331e3be315c0b845f1fd4634ff6d1b
Sharryy <ibneadam388@gmail.com>
2021-09-22 02:54:36 +0500

    Moved Skeleton to Blade Component

---
+++ resources/views/components/game-card-small-skeleton.blade.php
@@ -0,0 +1,9 @@
+<div class="game flex">
+    <div class="bg-gray-800 w-16 h-20 flex-none"></div>
+    <div class="ml-4">
+        <div class="text-transparent bg-gray-700 rounded leading-tight">title goes here today</div>
+        <div
+            class="text-transparent bg-gray-700 rounded inline-block text-sm mt-1">Sept 14, 2019
+        </div>
+    </div>
+</div>
--- resources/views/livewire/most-anticipated.blade.php
+++ resources/views/livewire/most-anticipated.blade.php
@@ -3,15 +3,7 @@
         <x-game-card-small :games="$games"/>
     @empty
         @foreach(range(1,4) as $games)
-            <div class="game flex">
-                <div class="bg-gray-800 w-16 h-20 flex-none"></div>
-                <div class="ml-4">
-                    <div class="text-transparent bg-gray-700 rounded leading-tight">title goes here today</div>
-                    <div
-                        class="text-transparent bg-gray-700 rounded inline-block text-sm mt-1">Sept 14, 2019
-                    </div>
-                </div>
-            </div>
+            <x-game-card-small-skeleton/>
         @endforeach
     @endforelse
 </div>
