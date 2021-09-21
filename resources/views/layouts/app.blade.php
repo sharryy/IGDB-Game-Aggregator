743cf5edf60d247bc785c13a12f25c2ba3beaa8f
Sharryy <ibneadam388@gmail.com>
2021-09-21 23:50:37 +0500

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laracasts Video Games</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
     <livewire:styles/>
 </head>
 <body class="bg-gray-900 text-white">
