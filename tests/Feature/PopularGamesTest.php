54723134c57db07e703c98bb23d1598f1f85196a
Sharryy <ibneadam388@gmail.com>
2021-09-23 15:04:11 +0500

    Fuck Testing

---
+++ tests/Feature/PopularGamesTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Livewire\PopularGames;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class PopularGamesTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        \Http::fake([
+            'https://api.igdb.com/v4/games' => \Http::response(['foo' => 'bar'], 200),
+        ]);
+
+        Livewire::test(PopularGames::class)
+            ->assertSet('popularGames', [])
+            ->call('loadPopularGames');
+    }
+}
