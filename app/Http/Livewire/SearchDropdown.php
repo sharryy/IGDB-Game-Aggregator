f2b4fc88210f318f4b4886d034175c7e48a6bcec
Sharryy <ibneadam388@gmail.com>
2021-09-25 03:00:19 +0500

    API Request Done

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class SearchDropdown extends Component
 {
+    public $search;
+    public $search_results = [];
+
     public function render()
     {
+        $this->search_results = Http::withHeaders(config('services.igdb'))
+            ->withBody("
+                search \"{$this->search}\";
+                fields name, slug, cover.url;
+                limit 6;
+            ", "text/plain")
+            ->post('https://api.igdb.com/v4/games')
+            ->json();
+
         return view('livewire.search-dropdown');
     }
 }
